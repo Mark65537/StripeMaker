c85f67f0b7e151a50e14b2ef1727065f5e3f07b8
mark65537 <grigorash-mark@mail.ru>
2025-12-02 17:01:16 +0700

    WIP: изменения размера изображения при изменении размера окна

--- StripeMaker/MainForm.h
+++ StripeMaker/MainForm.h
@@ -8,6 +8,7 @@ namespace StripeMaker {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System::Drawing::Drawing2D;
 	using namespace Code128Lib;
 
 	/// <summary>
@@ -31,11 +32,22 @@ namespace StripeMaker {
 			{
 				delete components;
 			}
+			if (m_lastBarcode != nullptr)
+			{
+				delete m_lastBarcode;
+				m_lastBarcode = nullptr;
+			}
+			if (pB_Main != nullptr && pB_Main->Image != nullptr)
+			{
+				delete pB_Main->Image;
+				pB_Main->Image = nullptr;
+			}
 		}
 	private: System::Windows::Forms::TextBox^ tB_Number;
 	private: System::Windows::Forms::PictureBox^ pB_Main;
 	private: System::Windows::Forms::Label^ l_Input;
 	private: System::Windows::Forms::Label^ l_Preview;
+	private: System::Drawing::Bitmap^ m_lastBarcode;
 	protected:
 
 	protected:
@@ -87,6 +99,7 @@ namespace StripeMaker {
 			this->pB_Main->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
 			this->pB_Main->TabIndex = 1;
 			this->pB_Main->TabStop = false;
+			this->pB_Main->Resize += gcnew System::EventHandler(this, &MainForm::pB_Main_Resize);
 			// 
 			// b_Convert
 			// 
@@ -136,6 +149,7 @@ namespace StripeMaker {
 			this->Name = L"MainForm";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"StripeMaker Ч генератор штрихкодов Code128";
+			this->Resize += gcnew System::EventHandler(this, &MainForm::MainForm_Resize);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pB_Main))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
@@ -148,14 +162,84 @@ namespace StripeMaker {
 		if (input->Length == 0) { MessageBox::Show("¬ведите цифры."); return; }
 		// TODO решить что делать с нечетной длиной
 		try {
-			Bitmap^ bmp = Code128Lib::Code128::GenerateCode128C(input, 2, 100, 10);
+			if (m_lastBarcode != nullptr) {
+				delete m_lastBarcode;
+				m_lastBarcode = nullptr;
+			}
 
-			pB_Main->Image = bmp;
-			pB_Main->SizeMode = PictureBoxSizeMode::CenterImage;
+			Bitmap^ bmp = Code128Lib::Code128::GenerateCode128C(input, 2, 100, 10);
+			m_lastBarcode = bmp;
+			UpdatePictureBoxImage();
 		}
 		catch (Exception^ ex) {
 			MessageBox::Show("ќшибка: " + ex->Message);
 		}
+	}
+
+	private: System::Void pB_Main_Resize(System::Object^ sender, System::EventArgs^ e) {
+		UpdatePictureBoxImage();
+	}
+
+	private: System::Void MainForm_Resize(System::Object^ sender, System::EventArgs^ e) {
+		UpdatePictureBoxImage();
+	}
+
+	private: Rectangle GetFitRectangle(System::Drawing::Size sourceSize, System::Drawing::Size targetSize) {
+		if (sourceSize.Width <= 0 || sourceSize.Height <= 0) {
+			return Rectangle(0, 0, targetSize.Width, targetSize.Height);
+		}
+
+		double scaleW = static_cast<double>(targetSize.Width) / static_cast<double>(sourceSize.Width);
+		double scaleH = static_cast<double>(targetSize.Height) / static_cast<double>(sourceSize.Height);
+		double scale = System::Math::Min(scaleW, scaleH);
+
+		int width = System::Math::Max(1, static_cast<int>(System::Math::Round(sourceSize.Width * scale)));
+		int height = System::Math::Max(1, static_cast<int>(System::Math::Round(sourceSize.Height * scale)));
+		int offsetX = (targetSize.Width - width) / 2;
+		int offsetY = (targetSize.Height - height) / 2;
+
+		return Rectangle(offsetX, offsetY, width, height);
+	}
+
+	private: Bitmap^ CreateFittedBarcodeBitmap(Bitmap^ sourceBarcode, System::Drawing::Size targetSize) {
+		if (sourceBarcode == nullptr || targetSize.Width <= 0 || targetSize.Height <= 0) {
+			return nullptr;
+		}
+
+		Bitmap^ canvas = gcnew Bitmap(targetSize.Width, targetSize.Height);
+		Graphics^ g = Graphics::FromImage(canvas);
+		try {
+			g->Clear(Color::White);
+			g->InterpolationMode = InterpolationMode::NearestNeighbor;
+			g->PixelOffsetMode = PixelOffsetMode::Half;
+			Rectangle dest = GetFitRectangle(sourceBarcode->Size, targetSize);
+			g->DrawImage(sourceBarcode, dest);
+		}
+		finally {
+			delete g;
+		}
+
+		return canvas;
+	}
+
+	private: System::Void UpdatePictureBoxImage(void) {
+		if (m_lastBarcode == nullptr || pB_Main == nullptr) {
+			return;
+		}
+
+		System::Drawing::Size targetSize = pB_Main->ClientSize;
+		Bitmap^ scaled = CreateFittedBarcodeBitmap(m_lastBarcode, targetSize);
+		if (scaled == nullptr) {
+			return;
+		}
+
+		if (pB_Main->Image != nullptr) {
+			delete pB_Main->Image;
+			pB_Main->Image = nullptr;
+		}
+
+		pB_Main->Image = scaled;
+		pB_Main->SizeMode = PictureBoxSizeMode::Normal;
 	}
 	};
 }

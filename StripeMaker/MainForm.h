34787b2ab72891a9cc13499b5e8eece058e3e657
mark65537 <grigorash-mark@mail.ru>
2025-12-02 16:37:56 +0700

    сделал более красивый интерфейс

--- StripeMaker/MainForm.h
+++ StripeMaker/MainForm.h
@@ -19,9 +19,6 @@ namespace StripeMaker {
 		MainForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: добавьте код конструктора
-			//
 		}
 
 	protected:
@@ -37,6 +34,8 @@ namespace StripeMaker {
 		}
 	private: System::Windows::Forms::TextBox^ tB_Number;
 	private: System::Windows::Forms::PictureBox^ pB_Main;
+	private: System::Windows::Forms::Label^ l_Input;
+	private: System::Windows::Forms::Label^ l_Preview;
 	protected:
 
 	protected:
@@ -60,44 +59,83 @@ namespace StripeMaker {
 			this->tB_Number = (gcnew System::Windows::Forms::TextBox());
 			this->pB_Main = (gcnew System::Windows::Forms::PictureBox());
 			this->b_Convert = (gcnew System::Windows::Forms::Button());
+			this->l_Input = (gcnew System::Windows::Forms::Label());
+			this->l_Preview = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pB_Main))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// tB_Number
 			// 
-			this->tB_Number->Location = System::Drawing::Point(12, 12);
+			this->tB_Number->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->tB_Number->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10));
+			this->tB_Number->Location = System::Drawing::Point(15, 32);
 			this->tB_Number->Name = L"tB_Number";
-			this->tB_Number->Size = System::Drawing::Size(373, 20);
+			this->tB_Number->Size = System::Drawing::Size(371, 25);
 			this->tB_Number->TabIndex = 0;
 			// 
 			// pB_Main
 			// 
-			this->pB_Main->Location = System::Drawing::Point(12, 76);
+			this->pB_Main->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->pB_Main->BackColor = System::Drawing::SystemColors::Window;
+			this->pB_Main->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->pB_Main->Location = System::Drawing::Point(15, 108);
 			this->pB_Main->Name = L"pB_Main";
-			this->pB_Main->Size = System::Drawing::Size(373, 178);
+			this->pB_Main->Size = System::Drawing::Size(571, 190);
+			this->pB_Main->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
 			this->pB_Main->TabIndex = 1;
 			this->pB_Main->TabStop = false;
 			// 
 			// b_Convert
 			// 
-			this->b_Convert->Location = System::Drawing::Point(13, 39);
+			this->b_Convert->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->b_Convert->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9));
+			this->b_Convert->Location = System::Drawing::Point(392, 31);
 			this->b_Convert->Name = L"b_Convert";
-			this->b_Convert->Size = System::Drawing::Size(87, 23);
+			this->b_Convert->Size = System::Drawing::Size(194, 27);
 			this->b_Convert->TabIndex = 2;
-			this->b_Convert->Text = L"Генерировать";
+			this->b_Convert->Text = L"Сгенерировать штрихкод";
 			this->b_Convert->UseVisualStyleBackColor = true;
 			this->b_Convert->Click += gcnew System::EventHandler(this, &MainForm::b_Convert_Click);
+			// 
+			// l_Input
+			// 
+			this->l_Input->AutoSize = true;
+			this->l_Input->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9));
+			this->l_Input->Location = System::Drawing::Point(12, 12);
+			this->l_Input->Name = L"l_Input";
+			this->l_Input->Size = System::Drawing::Size(171, 15);
+			this->l_Input->TabIndex = 3;
+			this->l_Input->Text = L"Введите число / код для C128:";
+			// 
+			// l_Preview
+			// 
+			this->l_Preview->AutoSize = true;
+			this->l_Preview->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9));
+			this->l_Preview->Location = System::Drawing::Point(12, 88);
+			this->l_Preview->Name = L"l_Preview";
+			this->l_Preview->Size = System::Drawing::Size(157, 15);
+			this->l_Preview->TabIndex = 4;
+			this->l_Preview->Text = L"Предпросмотр штрихкода:";
 			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(398, 290);
+			this->BackColor = System::Drawing::Color::WhiteSmoke;
+			this->ClientSize = System::Drawing::Size(604, 321);
+			this->Controls->Add(this->l_Preview);
 			this->Controls->Add(this->b_Convert);
 			this->Controls->Add(this->pB_Main);
+			this->Controls->Add(this->l_Input);
 			this->Controls->Add(this->tB_Number);
+			this->Font = (gcnew System::Drawing::Font(L"Segoe UI", 8.25F));
+			this->MinimumSize = System::Drawing::Size(620, 360);
 			this->Name = L"MainForm";
-			this->Text = L"StripeMaker";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"StripeMaker — генератор штрихкодов Code128";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pB_Main))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
